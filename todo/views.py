29c76697cdff7c8be06379e2e05d1a46bdfdbec3
1F10220075 <s1f102200750@iniad.org>
2023-07-06 13:05:31 +0900

    requirements.txt 追加

--- todo/views.py
+++ todo/views.py
@@ -4,14 +4,19 @@ from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
 # Create your views here.
-def index(reqest):
-    if reqest.method == 'POST':
-        task = Task(title=reqest.POST['title'], due_at=make_aware(parse_datetime(reqest.POST['due_at'])))
+def index(request):
+    if request.method == 'POST':
+        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
     
-    tasks = Task.objects.all()
+    if request.GET.get('order') == 'due':
+        tasks = Task.objects.order_by('due_at')
+    else:
+        tasks = Task.objects.order_by('-posted_at')
+    
+    # tasks = Task.objects.all()
 
     context = {
         'tasks': tasks
     }
-    return render(reqest, 'todo/index.html', context)
+    return render(request, 'todo/index.html', context)
